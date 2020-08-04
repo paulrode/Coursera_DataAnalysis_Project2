a9d9e965ca699b69628530111a3f17051fd1ed77
paulrode <paulrode@outlook.com>
2020-08-04 18:36:13 -0400

    Question 1 progress

--- Question1.R
+++ Question1.R
@@ -1,4 +1,4 @@
-
+setup()
 
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
 # Using the base plotting system, make a plot showing the total PM2.5 emission from 
@@ -11,6 +11,7 @@ list.files("./data")
 nei <- readRDS("./data/summarySCC_PM25.rds")
 dim(nei)
 str(nei)
+summary(nei)
 head(nei)
 
 scc <- readRDS("./data/Source_Classification_Code.rds")
@@ -18,6 +19,16 @@ dim(scc)
 str(scc)
 head(scc)
 
-x0 <- subset(nei, year == 1999, select = c(fips, SCC, Pollutant, Emissions, type, year))
-with(nei, boxplot, Emissions, year)
-plot(nei$Emissions, nei$year)
+idx <- sample(nrow(nei), 100)
+
+ 
+nei %>% group_by(year) %>% summarise(pmtons = sum(Emissions), pmmean = mean(Emissions)) -> nei1
+dim(nei1)
+nei1
+boxplot(log2(pmtons~year), data = nei1)
+boxplot(Emissions~year, data = nei)
+summary(nei1)
+
+
+
+
