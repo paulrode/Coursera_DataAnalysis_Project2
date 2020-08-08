6ef14a4c3a9e4a24b21305f7e88bf96210ecfd70
paulrode <paulrode@outlook.com>
2020-08-08 18:57:39 -0400

    Name Change

---
+++ Plot1.R
@@ -0,0 +1,40 @@
+setup()
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from 
+# all sources for each of the years 1999, 2002, 2005, and 2008.
+
+# List files and pull in data
+list.files("./data")
+
+## This first line will likely take a few seconds. Be patient!
+nei <- readRDS("./data/summarySCC_PM25.rds")
+dim(nei)
+str(nei)
+summary(nei)
+
+
+scc <- readRDS("./data/Source_Classification_Code.rds")
+dim(scc)
+str(scc)
+head(scc)
+
+idx <- sample(nrow(nei), 100)
+
+ 
+nei %>% group_by(year) %>% summarise(pm_tons = sum(Emissions), pm_mean = mean(Emissions)) -> nei1
+dim(nei1)
+nei1
+with(nei1, plot(year, pm_tons, ylab = "total pm tons"), xlab = "year", main = "Question1")
+with(nei1, lines(year, pm_tons))
+title("Question 1")
+
+view(scc$Short.Name)
+view(scc$EI.Sector)
+scc$Option.Group
+scc$Option.Set
+scc$SCC.Level.One
+scc$SCC.Level.Two
+scc$SCC.Level.Three
+scc$SCC.Level.Four
+scc$Map.To
---
+++ Plot5.R
@@ -0,0 +1,32 @@
+# How have emissions from motor vehicle sources changed from 1999–2008 
+# in Baltimore City?
+
+setup()
+
+nei <- readRDS("./data/summarySCC_PM25.rds")
+dim(nei)
+str(nei)
+glimpse(nei)
+summary(nei)
+
+scc <- readRDS("./data/Source_Classification_Code.rds")
+dim(scc)
+glimpse(scc)
+
+mobile <- filter(scc, grepl("Mobile", EI.Sector))
+dim(mobile)
+glimpse(mobile)
+
+nei %>% filter(fips == "24510" & type == "ON-ROAD" ) %>%   semi_join(mobile, by="SCC") -> nei3
+nei3$year <- as.character(nei3$year)
+dim(nei3)
+glimpse(nei3)
+
+
+ggplot(data = nei3, aes(x=year, y=Emissions )) +
+  geom_col() +
+  labs(title = "Question 5", subtitle = "Explortoty Data") +
+  theme_light()
+
+
+
